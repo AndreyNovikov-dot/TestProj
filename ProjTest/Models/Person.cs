01e5257f74ca819f3891a5fd28bfeecb3c2ab75a
Andrew Novikov <012andrew12o@gmail.com>
2021-01-05 10:28:34 +0400

    Update

--- ProjTest/Models/Person.cs
+++ ProjTest/Models/Person.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
 using System;
-using System.Collections.Generic;
+
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace ProjTest.Models
 {
--- ProjTest/Models/PersonRecord.cs
+++ ProjTest/Models/PersonRecord.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
+
 
 
 
